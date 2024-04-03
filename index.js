2b3220506c98f20e4a07f9269aa7b0bcd40eb00f
Avishek Behera <avishekbehera87@gmail.com>
2024-04-03 21:43:53 +0530

    (test): another test to check pre push from local

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.post("/login", (req, res) => {
     const responseData = { message: "Authenticated", email: email };
     res.status(200).json(responseData);
   } else {
-    const responseData = { message: "Invalid Credentials_bug2" };
+    const responseData = { message: "Invalid Credentials_bug3" };
     res.status(401).json(responseData);
   }
 });
